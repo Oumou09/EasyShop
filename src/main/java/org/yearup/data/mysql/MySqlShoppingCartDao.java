443a0242a887646c86bcf3eb77841fab6e45ccd6
Oumou09 <oumoudesigns@gmail.com>
2025-06-25 01:16:47 -0400

    I just finished the addProductToCart and UpdateProductQuantity and the deleteItemOffCart methods

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -7,6 +7,10 @@ import org.yearup.models.ShoppingCartItem;
 
 import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 @Component
 public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
@@ -22,11 +26,41 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
 
     @Override
     public void addProductToCart(int userId, int productId, int quantity) {
+        String sql = "INSERT INTO shopping_cart " +
+                "VALUES(?, ?, ?)";
+        try(Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, userId);
+            statement.setInt(2, productId);
+            statement.setInt(3,quantity);
+            statement.executeQuery();
+
+        }catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
     @Override
     public void updateProductQuantity(int userId, int productId, int quantity) {
+        String sql = "UPDATE shopping_cart" +
+                "SET user_id = ?" +
+                ", product_id = ?" +
+                "WHERE quantity = ?";
+
+        try(Connection connection = getConnection()){
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, userId);
+            statement.setInt(2, productId);
+            statement.setInt(3,quantity);
+            statement.executeUpdate();
+
+        }catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -42,11 +76,37 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
 
     @Override
     public void deleteItemOffCart(int productId) {
+        String sql = "DELETE FROM shopping_cart " +
+                " WHERE product_id = ?;";
+
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, productId);
+
+            statement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public ShoppingCart getByUserId(int userId) {
+        String sql = "SELECT * FROM shopping_cart WHERE user_id = ?";
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, userId);
+            statement.executeQuery();
+
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 }
